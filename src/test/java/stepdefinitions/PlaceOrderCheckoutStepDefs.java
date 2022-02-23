e81c3ceaaacc67659f2d17ba494f2c546a06c387
NaciyeAydin <naciyeaydinnn@gmail.com>
2022-02-23 21:38:45 +0100

    Automation Cucumber

---
+++ src/test/java/stepdefinitions/PlaceOrderCheckoutStepDefs.java
@@ -0,0 +1,73 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+public class PlaceOrderCheckoutStepDefs {
+    @Then("user clicks cart button")
+    public void user_clicks_cart_button() {
+
+    }
+
+
+    @Then("verifies that cart page is displayed")
+    public void verifiesThatCartPageIsDisplayed() {
+    }
+
+    @And("click proceed to checkout")
+    public void clickProceedToCheckout() {
+    }
+
+    @And("user clicks register login button")
+    public void userClicksRegisterLoginButton() {
+    }
+
+    @And("user fills all details in Signup and create account")
+    public void userFillsAllDetailsInSignupAndCreateAccount() {
+    }
+
+    @And("user verifies account created and clicks continue button")
+    public void userVerifiesAccountCreatedAndClicksContinueButton() {
+    }
+
+    @And("user verifies logged in as username at top")
+    public void userVerifiesLoggedInAsUsernameAtTop() {
+    }
+
+    @And("user  clicks cart button")
+    public void userClicksCartButton() {
+    }
+
+    @And("user clicks proceed to checkout button")
+    public void userClicksProceedToCheckoutButton() {
+    }
+
+    @And("user verifies Address Details and Review Your Order")
+    public void userVerifiesAddressDetailsAndReviewYourOrder() {
+    }
+
+    @And("user enters description in comment text area and click place order")
+    public void userEntersDescriptionInCommentTextAreaAndClickPlaceOrder() {
+    }
+
+    @And("user enters payment details Name on Card Card Number CVC Expiration date")
+    public void userEntersPaymentDetailsNameOnCardCardNumberCVCExpirationDate() {
+    }
+
+    @And("user click Pay and Confirm Order button")
+    public void userClickPayAndConfirmOrderButton() {
+    }
+
+    @And("user verifies success message Your order has been placed successfully")
+    public void userVerifiesSuccessMessageYourOrderHasBeenPlacedSuccessfully() {
+    }
+
+    @And("user click Delete Account button")
+    public void userClickDeleteAccountButton() {
+    }
+
+    @Then("user verifies account deleted and clicks Continue button")
+    public void userVerifiesAccountDeletedAndClicksContinueButton() {
+    }
+}
+
